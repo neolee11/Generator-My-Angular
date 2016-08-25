20a12edacef7efbcd7ee878a9c1af9d6c9eeeb58
Daniel Li <neolee11@yahoo.com>
2016-08-25 17:54:20 -0400

    progress

--- app/index.js
+++ app/index.js
@@ -2,4 +2,31 @@
  * Created by Weili on 8/25/2016.
  */
 
-test
+var generators = require('yeoman-generator');
+var _ = require('lodash');
+
+
+module.exports = generators.Base.extend({
+    constructor: function(){
+        generators.Base.apply(this, arguments);
+
+    },
+
+    initializing: function(){
+        console.log('init');
+    },
+    prompting: function(){
+    },
+    configuring: function(){
+    },
+    writing: function(){
+    },
+    conflicts: function(){
+    },
+    install: function(){
+    },
+    end: function(){
+    }
+
+
+});
